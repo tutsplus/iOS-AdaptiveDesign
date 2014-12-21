d641deef813a0afbdb5696594ccdaeb768e2fdde
Jordan Morgan <info@dreaminginbinary.co>
2014-12-20 20:05:08 -0600

    Initial Commit
    Starting point for the adaptive design tutorial found at
    code.tutsplus.com

---
+++ Adaptive_Apps/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Adaptive_Apps
+//
+//  Created by Jordan Morgan on 12/15/14.
+//  Copyright (c) 2014 Jordan Morgan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()        
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
